d5dafc04a7dc80cef46ac51bbc7c8c7591f78906
Kang Dhanie <73064217+sidhanie@users.noreply.github.com>
2021-07-26 03:37:07 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "ZTk3MjRlOGMtMTViNi00OTMwLTk5YjktYzhmY2RhYTExZjFk";
+$uuid = "3b0671f9cc1c9ebb";
+$userid = "30928260";
+
+$eksternal_version = "2.0.2";
+$version = "42";
+$device = "9";
+$timezone = "7";
+$lang = "id";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
